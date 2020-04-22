69963baf24a10c5d16654eef84ac1e8de7c96051
Sphinx <mattis@sevenfelicce.de>
2020-04-22 12:09:38 +0200

    Class diagrams added to documentation

--- template.h
+++ template.h
@@ -16,9 +16,13 @@ class global;
 #include "local";
 #include <global>;
 
+namespace acroynm {
+
 struct StructureName {
 	int variable;
 }
+
+enum MyTypes{};
 
 class ClassName {
 
@@ -30,18 +34,27 @@ public:
 	
 	void update();
 	
-	void get_variable();
+	int get_variable() const {return variable;}
 	
-	void set_variable();
+	void set_variable(int desired_variable);
+
+	bool is_binary() const {return binary;}
 
 	void arbitrary_method();
+
+	void quick_method();
 	
 private:
 	OtherClass *otherclass;
 
 	int variable;
+	bool binary;
 	
 	void secret_method();
 };
+
+inline void ClassName::quick_method() {}
+
+}
 
 #endif
