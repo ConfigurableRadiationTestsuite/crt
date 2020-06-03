0a8da4d5395e821d1fc241bb0af91897dc98dd49
Sphinx <mattis@sevenfelicce.de>
2020-06-03 09:10:19 +0200

    Added QTcp newtork capability

--- src/Tabs/ETHTab.cpp
+++ src/Tabs/ETHTab.cpp
@@ -24,20 +24,16 @@ void ETHTab::load_from_config() {
 
 void ETHTab::create_subwindow_from_dialog() {
     QString name = addDialog->get_entry_list()[0].input_value->text();
-    QString address = addDialog->get_entry_list()[1].input_value->text();
-    uint port = addDialog->get_entry_list()[3].input_value->text().toUInt();
-    QString folder = addDialog->get_entry_list()[3].input_value->text();
-    long long timeout = addDialog->get_entry_list()[4].input_value->text().toULongLong();
+    uint port = addDialog->get_entry_list()[1].input_value->text().toUInt();
+    long long timeout = addDialog->get_entry_list()[2].input_value->text().toULongLong();
 
-    subWindow_list.push_back(new ETHW(runManager, new Ethernet(runManager, name, address, port, folder, timeout)));
+    subWindow_list.push_back(new ETHW(runManager, new Ethernet(runManager, name, port, timeout)));
 
     update_layout();
 }
 
 void ETHTab::create_add_subwindow_dialog() {
     addDialog->add_entry("Name", "Temperature Data");
-    addDialog->add_entry("Address", "192.168.3.103");
     addDialog->add_entry("Port", "9010");
-    addDialog->add_entry("Folder", "AAAAH");
     addDialog->add_entry("Timeout", "9999");
 }
