ef86353f47fdd2e3f07e2f34928c4834e70b0c75
Sphinx <mattis@sevenfelicce.de>
2020-11-18 08:17:49 +0100

    Added Fieldfox plot

---
+++ src/Plot/FFOXPlot.cpp
@@ -0,0 +1,53 @@
+#include "FFOXPlot.h"
+
+#include "src/Components/Fieldfox.h"
+
+FFOXPlot::FFOXPlot(QCustomPlot *m_plot, int m_datapoints, int m_seconds)
+    : Plot(m_plot, m_datapoints, m_seconds) {
+    create_layout();
+
+    recreate_axis(xAxis);
+}
+
+FFOXPlot::~FFOXPlot() {}
+
+void FFOXPlot::update_plot() {
+    /* Set y-axis */
+    int minimum = get_minimum(data);
+    plot->yAxis->setRange(minimum, 0);
+
+    //Set data
+    plot->graph(0)->setData(xAxis, data);
+
+    plot->replot();
+}
+
+void FFOXPlot::update_data(const QVector<double> &data) {
+    this->data = data;
+
+    if(this->data.size() != xAxis.size())
+        recreate_axis(xAxis);
+
+    update_plot();
+}
+
+void FFOXPlot::create_layout() {
+    plot->addGraph();
+    plot->graph(0)->setPen(QPen(Qt::blue));
+
+    plot->xAxis->setLabel("[ ]");
+    plot->xAxis->setRange(0, datapoints);
+
+    plot->yAxis->setLabel("[dBm]");
+    plot->yAxis->setRange(-100, 0);
+
+    plot->replot();
+}
+
+void FFOXPlot::recreate_axis(QVector<double> &vec) {
+    vec.clear();
+    vec.resize(datapoints);
+
+    for(int i = 0; i < datapoints; i++)
+        vec[i] = i;
+}
---
+++ src/Plot/FFOXPlot.h
@@ -0,0 +1,38 @@
+#ifndef FFOXPLOT_H
+#define FFOXPLOT_H
+
+/*
+ * Author: Mattis Jaksch
+ *
+ * Plotting the fieldfox spectrum analyzer graph
+ *
+ */
+
+class Fieldfox;
+
+#include "Plot.h"
+
+class FFOXPlot : public Plot {
+Q_OBJECT
+
+public:
+    FFOXPlot(QCustomPlot *m_plot, int m_datapoints=30, int m_seconds=30);
+    virtual ~FFOXPlot() override;
+
+public slots:
+    void update_plot() override;
+
+    void update_data(const QVector<double> &data);
+
+private:
+    QVector<double> xAxis;
+    QVector<double> data;
+
+    QElapsedTimer *plot_timer;
+
+    void create_layout() override;
+
+    void recreate_axis(QVector<double> &vec) override;
+};
+
+#endif // FFOXPLOT_H
